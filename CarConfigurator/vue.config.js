a436c97c65f5a42c51de3b11e48fe7d3bf61f9d7
Kevin Glier <github@objement.com>
2021-04-05 22:16:52 +0200

    Change github workflow

--- CarConfigurator/vue.config.js
+++ CarConfigurator/vue.config.js
@@ -1,6 +1,7 @@
 ﻿const path = require("path");
 
 module.exports = {
+    filenameHashing: false,
     configureWebpack: config => {
         if (process.env.NODE_ENV === "production") {
             config.output.filename = '[name].min.js'
